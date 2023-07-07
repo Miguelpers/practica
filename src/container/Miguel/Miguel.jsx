0dfba288596e881036b5fa4d99a387edce5bee7f
Miguelpers <137926338+Miguelpers@users.noreply.github.com>
2023-07-07 09:30:23 -0430

    Ejercicio 1

--- src/container/Miguel/Miguel.jsx
+++ src/container/Miguel/Miguel.jsx
@@ -11,6 +11,21 @@ const Miguel = () => {
 			</div>
 			<div className="app__miguel-excerpt">
 				<p className="p-text"></p>
+				<div> 
+				 <img src="https://dummyimage.com/200x200/000/fff" alt="imagen 1"/>  
+					<span> Según estudio, el nivel de felicidad de una persona promedio disminuye después de los 21 años </span>
+					
+				</div>
+				<div> 
+				  <img src="https://dummyimage.com/200x200/000/fff" alt="imagen 1"/>  
+					<span> Según estudio, el nivel de felicidad de una persona promedio disminuye después de los 21 años </span>
+					
+				</div>
+				<div> 
+				  <img src="https://dummyimage.com/200x200/000/fff" alt="imagen 1"/>  
+					<span> Según estudio, el nivel de felicidad de una persona promedio disminuye después de los 21 años </span>
+					
+				</div>
 			</div>
 		</div>
     </div>
