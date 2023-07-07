de5e83279f2590471f683ab324a66b8f946502e0
Miguelpers <137926338+Miguelpers@users.noreply.github.com>
2023-07-07 10:07:20 -0430

    Cambios Miguel

--- src/container/Miguel/Miguel.jsx
+++ src/container/Miguel/Miguel.jsx
@@ -10,8 +10,38 @@ const Miguel = () => {
 			    <h1>Miguel</h1>
 			</div>
 			<div className="app__miguel-excerpt">
+<<<<<<< Updated upstream
 				<p className="p-text">1234565768</p>
 				<h2>Nelson></h2>
+=======
+				<p className="p-text"></p>
+				<div> 
+				 <img src="https://dummyimage.com/200x200/000/fff" alt="imagen 1"/>  
+					<span> Según estudio, el nivel de felicidad de una persona promedio disminuye después de los 21 años </span>
+					
+				</div>
+				<div> 
+				  <img src="https://dummyimage.com/200x200/000/fff" alt="imagen 1"/>  
+					<span> Según estudio, el nivel de felicidad de una persona promedio disminuye después de los 21 años </span>
+					
+				</div>
+				<div> 
+				  <img src="https://dummyimage.com/200x200/000/fff" alt="imagen 1"/>  
+					<span> Según estudio, el nivel de felicidad de una persona promedio disminuye después de los 21 años </span>
+					
+				</div>
+				<div className="container">
+					<section className="card">
+						<div className="card_header">
+							<img className="card_image_header" src="https://img.freepik.com/fotos-premium/triste-infeliz-llorando-mujer-llorando-ojos-lagrimas_122732-2274.jpg" alt="Mujer llorando"/>
+						</div>
+						<div class="card_body">
+							<p class="text_body"> Las mijeres lloran entre 30 y 60 veces al año, mientras derraman lagrimas entre 7 y 17 veces al año.  </p>
+						</div>
+					</section>	
+				</div>
+				
+>>>>>>> Stashed changes
 			</div>
 		</div>
     </div>
